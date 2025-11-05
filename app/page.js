6bfde56aa554810d0a7c5aec7ad76d1e0aa35087
tetianakhytra <tekh0001@stud.kea.dk>
2025-11-05 22:08:45 +0100

    tailwind done

--- app/page.js
+++ app/page.js
@@ -11,56 +11,67 @@ export default function Page() {
   const [currentWatch, setCurrentWatch] = useState("/navy.png");
 
   return (
-    <div className="grid_container">
+ <div className="grid grid-cols-12 gap-10 w-full mx-auto px-10 items-center">
 
-      <div className="hero_text">
-        <h1>
-          The Perfect Moment<br />
-          <span>Between Past And Future.</span>
-        </h1>
+
+
+      <div className="col-start-1 col-end-8 row-start-2 row-end-4">
+  <h1 className="text-white font-poppins text-[64px] font-bold leading-[86px] tracking-[1.28px]">
+  The Perfect Moment
+  <br />
+  Between Past
+  <br />
+  <span className="font-normal">And Future.</span>
+</h1>
+
+
       </div>
 
-      <div className="big_watch">
+      <div className="col-start-6 col-end-13 row-start-2 row-end-4 flex justify-center ">
         <Image src={currentWatch} alt="big watch" width={486} height={486} />
       </div>
 
-      <div className="color_indicator">
+      <div className="flex flex-col justify-center gap-1 items-center col-start-12 col-end-13 row-start-2 row-end-4 self-center">
         <ColorIndicator
           currentWatch={currentWatch}
           setCurrentWatch={setCurrentWatch}
         />
       </div>
 
-      <div className="page_indicator">
+      <div className="col-start-1 row-start-4 flex items-center mt-8">
         <PageIndicator />
       </div>
 
-      <div className="hero_button">
-        <ButtonCat />
-      </div>
+    <div className="col-start-1 col-end-6 row-start-3 self-start">
+  <ButtonCat />
+</div>
 
-      <div className="small_watch_row">
-        <WatchCards
-          img="/navy.png"
-          className="bg-navy"
-          isActive={currentWatch === "/navy.png"}
-          onClick={() => setCurrentWatch("/navy.png")}
-        />
 
-        <WatchCards
-          img="/mint.png"
-          className="bg-mint"
-          isActive={currentWatch === "/mint.png"}
-          onClick={() => setCurrentWatch("/mint.png")}
-        />
+      <div className="flex gap-5 mt-[30px] col-start-7 col-end-12 row-start-4">
 
-        <WatchCards
-          img="/ocean.png"
-          className="bg-ocean"
-          isActive={currentWatch === "/ocean.png"}
-          onClick={() => setCurrentWatch("/ocean.png")}
-        />
-      </div>
+  <WatchCards
+    img="/navy.png"
+    bg="bg-[rgba(67,69,88,0.6)]"
+    isActive={currentWatch === "/navy.png"}
+    onClick={() => setCurrentWatch("/navy.png")}
+  />
+
+  <WatchCards
+    img="/mint.png"
+    bg="bg-[rgba(106,221,204,0.6)]"
+    isActive={currentWatch === "/mint.png"}
+    onClick={() => setCurrentWatch("/mint.png")}
+  />
+
+  <WatchCards
+    img="/ocean.png"
+    bg="bg-[rgba(249,205,196,0.6)]"
+    isActive={currentWatch === "/ocean.png"}
+    onClick={() => setCurrentWatch("/ocean.png")}
+  />
+
+</div>
+
     </div>
   );
 }
--- components/WatchCards.jsx
+++ components/WatchCards.jsx
@@ -1,15 +1,20 @@
-
-
 "use client";
 
-export default function WatchCards({ img, onClick, className }) {
+export default function WatchCards({ img, onClick, isActive, bg }) {
   return (
-    <div className={`small-watch-card ${className}`} onClick={onClick}>
-      <img src={img} />
+    <div
+      onClick={onClick}
+      className={`
+        w-[131px] h-[70px] rounded-[10px]
+        flex items-center justify-center cursor-pointer
+        transition-all duration-200 hover:scale-105
+        ${bg}
+      `}
+    >
+      <img
+        src={img}
+        className="w-[92px] h-[90px] -mt-10"
+      />
     </div>
   );
 }
-
-
-  
-
